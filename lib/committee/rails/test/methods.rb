b819665e3635853a9a6dd92ebf66708495c66123
ota42y <ota42y@gmail.com>
2018-11-15 23:14:15 +0900

    support committee 3.0.0

--- lib/committee/rails/test/methods.rb
+++ lib/committee/rails/test/methods.rb
@@ -3,65 +3,30 @@ module Committee::Rails
     module Methods
       include Committee::Test::Methods
 
-      def committee_schema
-        @committee_schema = committee_options[:schema]
-
-        @committee_schema ||= begin
-
-          driver = Committee::Drivers::HyperSchema.new
-          schema_hash = JSON.parse(File.read(Rails.root.join('docs', 'schema', 'schema.json')))
-          driver.parse(schema_hash)
-        end
-      end
-
       def committee_options
-        {}
+        @committee_options ||= build_comittee_option
       end
 
-      def assert_schema_conform
-        @committee_schema ||= begin
-          # The preferred option. The user has already parsed a schema elsewhere
-          # and we therefore don't have to worry about any performance
-          # implications of having to do it for every single test suite.
-          if committee_schema
-            committee_schema
-          else
-            schema = schema_contents
+      def request_object
+        request
+      end
 
-            if schema.is_a?(String)
-              warn_string_deprecated
-            elsif schema.is_a?(Hash)
-              warn_hash_deprecated
-            end
+      def response_data
+        [response.status, response.headers, response.body]
+      end
 
-            if schema.is_a?(String)
-              schema = JSON.parse(schema)
-            end
+      private
 
-            if schema.is_a?(Hash) || schema.is_a?(JsonSchema::Schema)
-              driver = Committee::Drivers::HyperSchema.new
+      def build_comittee_option
+        if RSpec.configuration.committee_schema_type == :hyper_schema
+          driver = Committee::Drivers::HyperSchema.new
+          schema = driver.parse(RSpec.configuration.committee_schema_data)
 
-              # The driver itself has its own special cases to be able to parse
-              # either a hash or JsonSchema::Schema object.
-              schema = driver.parse(schema)
-            end
+          { schema: schema }
+        elsif RSpec.configuration.committee_schema_type == :open_api_3
+          open_api = OpenAPIParser.parse(RSpec.configuration.committee_schema_data)
 
-            schema
-          end
-        end
-
-        @committee_router ||= Committee::Router.new(@committee_schema,
-          prefix: schema_url_prefix)
-
-        link, _ = @committee_router.find_request_link(request)
-        unless link
-          invalid_response = "`#{request.request_method} #{request.path_info}` undefined in schema."
-          raise Committee::InvalidResponse.new(invalid_response)
-        end
-
-        if validate_response?(response.response_code)
-          data = JSON.parse(response.body)
-          Committee::ResponseValidator.new(link).call(response.response_code, response.headers, data)
+          { open_api_3: Committee::Drivers::OpenAPI3.new.parse(open_api) }
         end
       end
     end
