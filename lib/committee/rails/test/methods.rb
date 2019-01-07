069fe343e81f8f64dfc1ef3381c544b493d104b1
ota42y <ota42y@gmail.com>
2019-01-07 19:04:03 +0900

    fix load method

--- lib/committee/rails/test/methods.rb
+++ lib/committee/rails/test/methods.rb
@@ -18,16 +18,13 @@ module Committee::Rails
       private
 
       def build_comittee_option
-        if RSpec.configuration.committee_schema_type == :hyper_schema
-          driver = Committee::Drivers::HyperSchema.new
-          schema = driver.parse(RSpec.configuration.committee_schema_data)
+        schema = if RSpec.configuration.committee_schema_json
+                   Committee::Drivers::load_from_json(RSpec.configuration.committee_schema_json)
+                 elsif RSpec.configuration.committee_schema_yaml
+                   Committee::Drivers::load_from_yaml(RSpec.configuration.committee_schema_yaml)
+                 end
 
-          { schema: schema }
-        elsif RSpec.configuration.committee_schema_type == :open_api_3
-          open_api = OpenAPIParser.parse(RSpec.configuration.committee_schema_data)
-
-          { open_api_3: Committee::Drivers::OpenAPI3.new.parse(open_api) }
-        end
+        { schema: schema }
       end
     end
   end
